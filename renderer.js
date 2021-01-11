d9bccb4c383a168898ff0d99d97f2a45b4c80c7d
choisukjune <sukjune.choi@gmail.com>
2021-01-11 18:31:01 +0900

    .

--- renderer.js
+++ renderer.js
@@ -82,6 +82,16 @@
 		window.resultFileList = {};
 
 		window.FNS = {};
+
+		window.FNS.logics = {
+			init : 0
+			, downloadHtml : 0
+			, getDetailLinks : 0
+			, downloadDetailHtml : 0
+			, detailHtmlToObject : 0
+			, resultJsonToHtml : 0
+		}
+
 		window.pageBaseUrl = "https://eomisae.co.kr/index.php?mid=os&page="
 		webview.addEventListener('did-finish-load', () => {
 
@@ -90,6 +100,26 @@
 			// console.log('currentURL is : ' + currentURL)
 			// console.log('titlePage is : ' + titlePage)
 
+			//-------------------------------------------------------;
+			//로직초기화
+			//-------------------------------------------------------;
+			window.FNS.init = function(){
+				window.YYMMDD_now = window.UTIL.DateFormat.YYMMDD();
+				window.YYYYMMDD = window.UTIL.DateFormat.YYYYMMDD();
+				var oneDayAgo_date = new Date();
+				oneDayAgo_date.setDate(oneDayAgo_date.getDate() - 2);
+				window.YYMMDD_oneDayAgo = window.UTIL.DateFormat.YYMMDD( oneDayAgo_date );
+		
+				window.maxPage = -1;
+				window.pageCnt = 1;
+				window._tmp = {}
+				window._tmp.cnt = 0;
+				window.linkList = [];
+				window.linkListKeys = [];
+				window.detailList = [];
+				window.resultFileList = {};
+			}
+			
 			//-------------------------------------------------------;
 			//페이지MAX걊 구하기;
 			//-------------------------------------------------------;
@@ -108,9 +138,11 @@
 			//-------------------------------------------------------;
 			//게시물HTML저장하기;
 			//-------------------------------------------------------;
-			window.FNS.downloadHtml = function( url ){
-
-				var dirPath = "./list/html/" + window.UTIL.DateFormat.YYYYMMDD() + "/"
+			window.FNS.downloadHtml = function( url, targetDate, cbFunction ){
+				
+				console.log( "[S] - window.FNS.downloadHtml - " +  window.pageCnt );
+				targetDate = targetDate || window.UTIL.DateFormat.YYYYMMDD();
+				var dirPath = "./list/html/" + targetDate + "/"
 				url = url || window.pageBaseUrl + window.pageCnt
 				webview.loadURL( url );
 				webview.executeJavaScript(`
@@ -126,21 +158,33 @@
 
 					//window.document.getElementsByClassName("card_content")[0].children[0].children[1].innerText
 					//window.document.getElementsByClassName("card_content")[0].children[1].children[0]
-					debugger;
+
 					var date = window.document.getElementsByClassName("card_content")[0].children[0].children[1].innerText;
 
-					if( Number( date.replace(/\./gi,"") ) < Number( window.YYMMDD_now.replace(/\./gi,"") ) )
+					if( Number( date.replace(/\./gi,"") ) < Number( targetDate.substr(2,6) ) )
 					{
-						debugger;
+						window.document.getElementById("_tmp").innerHTML = "";
+						console.log( "[E] - window.FNS.downloadHtml - 큼" +  window.pageCnt );
+						cbFunction( targetDate );
+					}
+					else if( Number( date.replace(/\./gi,"") ) > Number(  targetDate.substr(2,6) ) )
+					{
+						console.log( "[E] - window.FNS.downloadHtml - 작음" +  window.pageCnt )
+						
+						++window.pageCnt;
+						
+						window.FNS.downloadHtml( window.pageBaseUrl + window.pageCnt, targetDate, cbFunction );
 						window.document.getElementById("_tmp").innerHTML = "";
 					}
 					else
 					{
 						fs.mkdirSync( dirPath, { recursive: true } );
 						fs.writeFileSync( dirPath + window.pageCnt + ".html", _data, {flag : "w"} )
-
+						console.log( "[E] - window.FNS.downloadHtml - " +  window.pageCnt )
+						
 						++window.pageCnt;
-						window.FNS.downloadHtml( window.pageBaseUrl + window.pageCnt );
+						
+						window.FNS.downloadHtml( window.pageBaseUrl + window.pageCnt, targetDate, cbFunction );
 						window.document.getElementById("_tmp").innerHTML = "";
 					}
 
@@ -151,12 +195,15 @@
 			//-------------------------------------------------------;
 			//게시물상세페이지링크 추출 및 저장하기;
 			//-------------------------------------------------------;
-			window.FNS.getDetailLinks = function( targetDate ){
+			window.FNS.getDetailLinks = function( targetDate, cbFunction ){
+				
+				console.log( "[S] - window.FNS.getDetailLinks" )
+
 				targetDate = targetDate || window.UTIL.DateFormat.YYYYMMDD();
 				var targetDirPath = targetDirPath || "./list/html/" + targetDate + "/";
 				var resultDirPath = resultDirPath || "./list/json/" + targetDate + "/";
 				var list = global.fs.readdirSync( targetDirPath );
-debugger;
+
 				var r = {};
 				var z = 0,zLen=list.length,zo;
 				for(;z<zLen;++z)
@@ -174,13 +221,13 @@ debugger;
 						var href = io.children[1].children[0].href
 
 						if( Number( date.replace(/\./gi,"") ) < Number( targetDate.substr(2,6) ) ) break;
+						if( Number( date.replace(/\./gi,"") ) > Number( targetDate.substr(2,6) ) ) continue;
 
 						console.log( date + " - " + href );
 
-						console.log( io.parentElement.children[0].children[0].src )
+						//console.log( io.parentElement.children[0].children[0].src )
 						if( io.parentElement.children[0].children[0].src ==  "http://eomisae.co.kr/images/123.png" )
 						{
-							debugger;
 							console.log( date + " - 아직열리지않음 - " + href );
 							continue;
 						}
@@ -190,25 +237,115 @@ debugger;
 							, img : io.parentElement.children[0].children[0].src
 						};
 					}
-					try
-					{
-						fs.mkdirSync( resultDirPath, { recursive: true } );
-						fs.writeFileSync( resultDirPath + window.UTIL.DateFormat.YYYYMMDD() + ".json", JSON.stringify( r ,null,4 ), {flag:"w"} );
-						window.document.getElementById("_tmp").innerHTML = "";
-					}
-					catch(er)
-					{
-						console.log( er );
-					}
+				}
 
+				
+				try
+				{
+					fs.mkdirSync( resultDirPath, { recursive: true } );
+					fs.writeFileSync( resultDirPath + targetDate + ".json", JSON.stringify( r ,null,4 ), {flag:"w"} );
+					window.document.getElementById("_tmp").innerHTML = "";
+					console.log( "[E] - window.FNS.getDetailLinks" )
+					cbFunction( targetDate );
+				}
+				catch(er)
+				{
+					console.log( er );
 				}
 			}
+			//-------------------------------------------------------;
+			//게시물상세페이지HTML 추출 및 저장하기;
+			//-------------------------------------------------------;
+			window.FNS.downloadDetailHtml = function( targetDate, cbFunction ){
+				console.log( "[S] - window.FNS.downloadDetailHtml - " + window._tmp.cnt )
+				targetDate = targetDate || window.UTIL.DateFormat.YYYYMMDD();
+				console.log( targetDate )
+				var targetFilePath = targetFilePath || "./list/json/" + targetDate + "/" + targetDate + ".json";
+				var resultDirPath = resultDirPath || "./detail/html/" + targetDate + "/";
 
+				if( window.linkList.length == 0 )
+				{
+					window.linkList = JSON.parse( global.fs.readFileSync( targetFilePath ).toString() );
+					window.linkListKeys = Object.keys( window.linkList );
+					fs.mkdirSync( resultDirPath, { recursive: true } );
+					var _ta = global.fs.readdirSync( resultDirPath );
+					var i = 0,iLen = _ta.length,io;
+					for(;i<iLen;++i){
+						io = _ta[ i ].split(".")[0];
+						window.resultFileList[ io ] = {}
+					}
+					window._tmp.cnt = 0
+				}
+
+				if( window.linkListKeys.length == window._tmp.cnt )
+				{
+					console.log( "[E] - window.FNS.downloadDetailHtml - " + window._tmp.cnt )
+					cbFunction( targetDate );
+					return;
+				}
+				var _t = window.linkList[ window.linkListKeys[ window._tmp.cnt ] ]
+
+				/*/
+				var xhr = new XMLHttpRequest();
+				xhr.onreadystatechange = function() { // 요청에 대한 콜백
+				  if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
+				    if (xhr.status === 200 || xhr.status === 201) {
+
+							var data = xhr.responseText;
+					   		var _data = data.replace(/\/\/img/gi, "https://img");
+
+							fs.mkdirSync( resultDirPath, { recursive: true } );
+  					 		fs.writeFileSync( resultDirPath + window.linkListKeys[ window._tmp.cnt ] + ".html", _data, {flag : "w"} )
+
+							console.log( "파일생성완료!" )
+
+  							++window._tmp.cnt;
+
+  							setTimeout(function(){ window.FNS.downloadDetailHtml(); },1000)
+
+
+
+				    } else {
+				      console.error(xhr.responseText);
+				    }
+				  }
+				};
+				xhr.open('GET', _t.url ); // 메소드와 주소 설정
+				xhr.send(); // 요청 전송
+				// xhr.abort(); // 전송된 요청 취소
+
+				/*/
+				if( !window.resultFileList[  window.linkListKeys[ window._tmp.cnt ] ] ){
+					debugger;
+					webview.loadURL( _t.url );
+					webview.executeJavaScript(`
+						var _el = window.document.getElementsByClassName("_wrapper")[0].innerHTML
+					 	Promise.resolve( _el )
+					 `).then(function(data){
+
+				 		var _data = data.replace(/\/\/img/gi, "https://img");
+				 		fs.mkdirSync( resultDirPath, { recursive: true } );
+				 		fs.writeFileSync( resultDirPath + window.linkListKeys[ window._tmp.cnt ] + ".html", _data, {flag : "w"} )
+						console.log( window._tmp.cnt + " / " + window.linkListKeys.length + " - " + _t.url + " - 파일생성완료!" )
+						console.log( "[E] - window.FNS.downloadDetailHtml - " + window._tmp.cnt )
+						++window._tmp.cnt;
+						setTimeout(function(){ window.FNS.downloadDetailHtml( targetDate, cbFunction ); },1000)
+					})
+			 	}
+				else
+				{
+					console.log( window._tmp.cnt + " / " + window.linkListKeys.length + " - " + _t.url + " - 파일이존재함" )
+					console.log( "[E] - window.FNS.downloadDetailHtml - " + window._tmp.cnt )
+					++window._tmp.cnt;
+					setTimeout(function(){ window.FNS.downloadDetailHtml( targetDate, cbFunction ); },1000)
+				}
+				 //*/
+			}
 			//-------------------------------------------------------;
 			//상세페이지 HTML 데이터추출 및 파일 저장;
 			//-------------------------------------------------------;
-			window.FNS.detailHtmlToObject = function( targetDate ){
-
+			window.FNS.detailHtmlToObject = function( targetDate, cbFunction ){
+				console.log( "[S] - window.FNS.detailHtmlToObject" )
 				targetDate = targetDate || window.UTIL.DateFormat.YYYYMMDD();
 				var targetDirPath = targetDirPath || "./detail/html/" + targetDate + "/";
 				var resultDirPath = resultDirPath || "./detail/json/" + targetDate + "/";
@@ -219,7 +356,6 @@ debugger;
 					window.detailList = global.fs.readdirSync( targetDirPath );
 					window._tmp.cnt = 0
 				}
-debugger;
 				var a = []
 				var z = 0,zLen = window.detailList.length,zo;
 				for(;z<zLen;++z){
@@ -227,27 +363,26 @@ debugger;
 
 					window.document.getElementById("_tmp").innerHTML = "";
 					window.document.getElementById("_tmp").innerHTML = global.fs.readFileSync( targetDirPath + zo ).toString();
-					//debugger;
+
 					var el00 = window.document.getElementsByTagName("table")[0].children[1].children;
 					var el_title = window.document.getElementsByTagName("h2")[0].innerText;
 					var id = zo.split(".")[0];
 					if( !listObj[ id ])
 					{
 						console.log( id )
-						debugger;
+						//TODO
+						//예외처리 추가해야함;
 					}
 					else
 					{
 						var r = {
-								id : id
-								, info : {}
-								, detail : []
-								, thmbnail : listObj[ zo.split(".")[0] ].img
-								, title : el_title
-								, date : targetDate
-							};
-
-
+							id : id
+							, info : {}
+							, detail : []
+							, thmbnail : listObj[ zo.split(".")[0] ].img
+							, title : el_title
+							, date : targetDate
+						};
 
 						var i = 0,iLen = el00.length,io;
 						for(;i<iLen;++i){
@@ -261,7 +396,6 @@ debugger;
 						var i = 0,iLen = el01.length,io;
 						for(;i<iLen;++i){
 							io = el01[ i ]
-							//debugger;
 							r.detail.push( io.outerHTML );
 						}
 
@@ -272,9 +406,10 @@ debugger;
 
 				try
 				{
-					//debugger;
 					fs.mkdirSync( resultDirPath, { recursive: true } );
 					fs.writeFileSync( resultDirPath + targetDate + ".json", JSON.stringify( a ,null,4 ), {flag:"w"} );
+					console.log( "[E] - window.FNS.detailHtmlToObject" )
+					cbFunction( targetDate );
 				}
 				catch(er)
 				{
@@ -304,6 +439,7 @@ debugger;
 				},
 			*/
 			window.FNS.resultJsonToHtml = function( targetDate ){
+				console.log( "[S] - window.FNS.resultJsonToHtml" )
 					targetDate = targetDate || window.UTIL.DateFormat.YYYYMMDD();
 				var targetFilePath = targetFilePath || "./detail/json/" + targetDate + "/" + targetDate + ".json";
 				var resultDirPath = resultDirPath || "../HttpServer_Default/html/";
@@ -352,7 +488,7 @@ debugger;
 						}
 					}
 					//r += "<td>내용</td><td>" + io.detail.join("\n").replace( /rel\=\"xe_gallery\"/gi, "width='200'" ) + "</td>"
-
+					//${description}
 					r += `
 					<div class="card">
 						<div class="image">
@@ -365,7 +501,7 @@ debugger;
 						</div>
 
 						<div class="description" style="font-size:11px;word-break: break-all;">
-							${description}
+							
 						</div>
 						</div>
 						<div class="extra content">
@@ -391,101 +527,41 @@ debugger;
 
 				fs.mkdirSync( resultDirPath, { recursive: true } );
 				fs.writeFileSync( resultDirPath + targetDate + ".html", r, {flag : "w"} )
+				console.log( "[E] - window.FNS.resultJsonToHtml" )
 			}
-
-
 			//-------------------------------------------------------;
-			//게시물상세페이지HTML 추출 및 저장하기;
+			//전체로직실행;
 			//-------------------------------------------------------;
-			window.FNS.downloadDetailHtml = function( targetDate ){
+			window.FNS.FN00 = function( targetDate ){
+				
+				console.log( "[ S ] - window.FNS.logics" )
+				
+				window.FNS.init()
+				console.log( "--------------- window.FNS.downloadHtml ---------------" );
+				window.FNS.downloadHtml( null ,targetDate, function(){
+					console.log( "--------------- window.FNS.downloadHtml ---------------" );
+					console.log( "--------------- window.FNS.getDetailLinks ---------------" );
+					window.FNS.getDetailLinks(targetDate, function(){
+						console.log( "--------------- window.FNS.getDetailLinks ---------------" );
+						console.log( "--------------- window.FNS.downloadDetailHtml ---------------" );
+						window.FNS.downloadDetailHtml( targetDate, function(){
+							console.log( "--------------- window.FNS.downloadDetailHtml ---------------" );
+							console.log( "--------------- window.FNS.detailHtmlToObject ---------------" );
+							window.FNS.detailHtmlToObject( targetDate, function(){
+								console.log( "--------------- window.FNS.detailHtmlToObject ---------------" );
+								console.log( "--------------- window.FNS.resultJsonToHtml ---------------" );
+								window.FNS.resultJsonToHtml( targetDate )
+								console.log( "--------------- window.FNS.resultJsonToHtml ---------------" );
+							});
+						})
+					});
+				} )
 
-				targetDate = targetDate || window.UTIL.DateFormat.YYYYMMDD();
-				var targetFilePath = targetFilePath || "./list/json/" + targetDate + "/" + targetDate + ".json";
-				var resultDirPath = resultDirPath || "./detail/html/" + targetDate + "/";
-
-				if( window.linkList.length == 0 )
-				{
-					window.linkList = JSON.parse( global.fs.readFileSync( targetFilePath ).toString() );
-					window.linkListKeys = Object.keys( window.linkList );
-					fs.mkdirSync( resultDirPath, { recursive: true } );
-					var _ta = global.fs.readdirSync( resultDirPath );
-					var i = 0,iLen = _ta.length,io;
-					for(;i<iLen;++i){
-						io = _ta[ i ].split(".")[0];
-						window.resultFileList[ io ] = {}
-					}
-					window._tmp.cnt = 0
-				}
-
-				if( window.linkListKeys.length == window._tmp.cnt )
-				{
-					debugger;
-					return;
-				}
-
-				console.log( window._tmp.cnt + " / " + window.linkListKeys.length )
-
-				var _t = window.linkList[ window.linkListKeys[ window._tmp.cnt ] ]
-
-				console.log( _t.url )
-
-				/*/
-				var xhr = new XMLHttpRequest();
-				xhr.onreadystatechange = function() { // 요청에 대한 콜백
-				  if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
-				    if (xhr.status === 200 || xhr.status === 201) {
-
-							var data = xhr.responseText;
-					   		var _data = data.replace(/\/\/img/gi, "https://img");
-
-							fs.mkdirSync( resultDirPath, { recursive: true } );
-  					 		fs.writeFileSync( resultDirPath + window.linkListKeys[ window._tmp.cnt ] + ".html", _data, {flag : "w"} )
-
-							console.log( "파일생성완료!" )
-
-  							++window._tmp.cnt;
-
-  							setTimeout(function(){ window.FNS.downloadDetailHtml(); },1000)
-
-
-
-				    } else {
-				      console.error(xhr.responseText);
-				    }
-				  }
-				};
-				xhr.open('GET', _t.url ); // 메소드와 주소 설정
-				xhr.send(); // 요청 전송
-				// xhr.abort(); // 전송된 요청 취소
-
-				/*/
-				debugger;
-				if( !window.resultFileList[  window.linkListKeys[ window._tmp.cnt ] ] ){
-
-					webview.loadURL( _t.url );
-					webview.executeJavaScript(`
-						var _el = window.document.getElementsByClassName("_wrapper")[0].innerHTML
-					 	Promise.resolve( _el )
-					 `).then(function(data){
-
-				 		var _data = data.replace(/\/\/img/gi, "https://img");
-				 		fs.mkdirSync( resultDirPath, { recursive: true } );
-				 		fs.writeFileSync( resultDirPath + window.linkListKeys[ window._tmp.cnt ] + ".html", _data, {flag : "w"} )
-						console.log( "파일생성완료!" )
-						++window._tmp.cnt;
-						setTimeout(function(){ window.FNS.downloadDetailHtml(); },1000)
-					})
-			 	}
-				else
-				{
-					console.log( "파일이존재함" )
-					++window._tmp.cnt;
-					window.FNS.downloadDetailHtml();
-				}
-				 //*/
+			
 			}
 		})
 	}
 })()
+
 //https://www.zerocho.com/category/HTML&DOM/post/594bc4e9991b0e0018fff5ed
 //xhr사용법
